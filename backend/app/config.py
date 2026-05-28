a6468a1f577a20465bb120ef5067e0b7e7801dcb
stazzyedev <aisacostaa@icloud.com>
2026-05-28 09:58:35 -0300

    fix: use database url only

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,16 +1,14 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
+    DATABASE_URL: str
 
     ORS_API_KEY: str = ""
     CORS_ORIGINS: str = "*"
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
